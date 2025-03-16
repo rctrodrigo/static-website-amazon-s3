d030af55d88d0e96f5421f29e65df053798f1f1a
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-03-16 23:17:44 +0800

    Add title tag content and favicon

---
+++ script/site.js
@@ -0,0 +1,5 @@
+// Set title tag content
+const author = "Raven Rodrigo";
+const projectTitle = "Portfolio Static Website";
+const titleTagContent = author + " | " + projectTitle;
+document.title = titleTagContent;
