a6e36fda3fd39c2718ad6e79bf84b40bc71ac9ea
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-03-17 17:24:15 +0800

    feat: add footer section

--- script/site.js
+++ script/site.js
@@ -3,3 +3,9 @@ const author = "Raven Rodrigo";
 const projectTitle = "Portfolio Static Website";
 const titleTagContent = author + " | " + projectTitle;
 document.title = titleTagContent;
+
+/* Footer */
+let footer = document.querySelector("footer");
+let copyrightYear = new Date().getFullYear();
+const footerSpan = `<span id="copyright">© ${copyrightYear} ${author}</span>`;
+footer.innerHTML = footerSpan;
