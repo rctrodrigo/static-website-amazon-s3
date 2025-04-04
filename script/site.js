e11c6abe770d83c60e302cc8f4967e8598a6b7f1
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2025-04-04 21:57:05 +0800

    feat: add footer section
    Used the DOM to select the footer element and a variable that store
    the current full year using Date API. The feature named template
    literals used as placeholders for current year and author name.

--- script/site.js
+++ script/site.js
@@ -3,3 +3,11 @@ const author = "Raven Rodrigo";
 const projectTitle = "Portfolio Static Website";
 const titleTagContent = author + " | " + projectTitle;
 document.title = titleTagContent;
+
+/* Footer */
+let footer = document.querySelector("footer");
+let copyrightYear = new Date().getFullYear();
+const footerSpan = `<span id="copyright">© ${copyrightYear} ${author}</span>`;
+footer.innerHTML = footerSpan;
+
+const footerElement = document.createElement("footer");
